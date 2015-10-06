7e986cec32610807dfa3114f9f037ffecf19efe6
scorpion1 <shgr8rb@gmail.com>
2015-10-06 17:27:47 +0500

    up and running

--- app/src/main/java/com/mydatingapp/SkStaticData.java
+++ app/src/main/java/com/mydatingapp/SkStaticData.java
@@ -1,9 +1,6 @@
 package com.mydatingapp;
 
-/**
- * Created by jk on 3/2/15.
- */
 public class SkStaticData {
-    public static String SITE_URL = null;
+    public static String SITE_URL = "http://datingt123.esy.es/";
     public static String  base64EncodedPublicKey = "";
 }
